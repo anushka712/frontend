772f1ef577dd331e5b85f804932042871b87e0fc
anushka712 <103297378+anushka712@users.noreply.github.com>
2024-02-22 16:07:00 +0000

    shorten url

---
+++ urlshortner/longurl.js
@@ -0,0 +1,9 @@
+let btn=document.getElementById("shorten");
+
+btn.addEventListener('click',short);
+
+function short(){
+    let longurl=document.getElementById('longURL').value;
+    
+    // console.log(longurl);
+}
