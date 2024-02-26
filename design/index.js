545f5c3671031d57935f509b9c4d6d80736e20d0
anushka712 <103297378+anushka712@users.noreply.github.com>
2024-02-26 15:26:52 +0000

    added some design

---
+++ design/index.js
@@ -0,0 +1,10 @@
+// function showdrop1(){
+//     document.getElementById("dropcontent1").style.display='block';
+  
+// }
+
+// function hidedrop1(){
+//     document.getElementById("dropcontent1").style.display='none';
+// }
+
+
