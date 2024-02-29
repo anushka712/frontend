e6d8a5a4e11cbdfb9e9e754eaf09bf23ad9718c3
anushka712 <aryalanushka2020@gmail.com>
2024-02-29 21:46:45 +0545

    signup page
    added the signup page

--- design/index.js
+++ design/index.js
@@ -8,3 +8,13 @@
 // }
 
 
+var modal = document.getElementById('login_main');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
